9f1d52658682fe28224f26d0847a7d02dbe99e93
Tintin V <tintin.vocard@>
2024-12-25 14:42:42 +0100

    aws/powershell : ajouter script de deploiement auto

---
+++ scripts/deploy.ps1
@@ -0,0 +1,22 @@
+# prerequisite :
+#   aws sdk configured locally
+# use me :
+#   .\deploy.ps1 MaLambda25
+
+
+param(
+    [string]$lambdaName
+)
+
+if (!$lambdaName) {
+    Write-Host "/!\ you didn't insert lambda name"
+    exit
+} else {
+    Write-Output "lambda with name: " $lambdaName
+}
+
+Write-Output "packaging..." $lambdaName
+mvn clean package --file ..\pom.xml
+
+Write-Output "deploying..." $lambdaName
+aws lambda update-function-code --function-name $lambdaName --zip-file "fileb://..\target\examples-1.jar"
--- src/main/java/example/HandlerIntegerJava17.java
+++ src/main/java/example/HandlerIntegerJava17.java
@@ -6,13 +6,9 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 public class HandlerIntegerJava17 implements RequestHandler<IntegerRecord, Integer>{
 
 	@Override
-	/*
-	 * Takes in an InputRecord, which contains two integers and a String.
-	 * Logs the String, then returns the sum of the two Integers.
-	 */
 	public Integer handleRequest(IntegerRecord event, Context context)
 	{
-		System.out.println("been here, done that. ");
+		System.out.println("been here, done that. 2");
 		return 10;
 	}
 }
